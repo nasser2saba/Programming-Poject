761bc0d89da6fcf7376c1c425baa1ce94dff7815
Saba <nassersaba2@gmail.com>
2025-06-05 14:03:37 +0200

    Add medication.js

---
+++ backend/src/models/medication.js
@@ -0,0 +1,14 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Medication = sequelize.define('Medication', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  userId: { type: DataTypes.INTEGER, allowNull: false },
+  naam: { type: DataTypes.STRING, allowNull: false },
+  dosis: { type: DataTypes.STRING, allowNull: false },
+  frequentie: { type: DataTypes.STRING, allowNull: false },
+  startDate: { type: DataTypes.DATEONLY, allowNull: false },
+  endDate: { type: DataTypes.DATEONLY },
+});
+
+module.exports = Medication;
