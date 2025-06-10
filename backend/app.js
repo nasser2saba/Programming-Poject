bc67149dbf34c1efa9758409523e39560fecdf32
Saba <nassersaba2@gmail.com>
2025-06-10 13:48:05 +0200

    Update main files

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
+const medRoutes = require('./routes/medication');
+const authRoutes = require('./src/routes/auth');
 
-const authRoutes = require('./routes/auth');
-
+// Middleware om JSON te kunnen lezen
 app.use(express.json());
+
+// Gebruik onze auth routes onder /api/auth
 app.use('/api/auth', authRoutes);
 
+app.use('/api/medications', medRoutes);
+
 module.exports = app;
+
+   
+   
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,41 @@
-require('dotenv').config();
-const app = require('./app');
-const sequelize = require('./config/database');
+// Load environment variables
+require("dotenv").config();
+require('./src/models/user');
+require('./src/models/medication');
 
+const express = require("express");
+const cors = require("cors");
+const sequelize = require("./src/config/database");
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+const authRoutes = require("./src/routes/auth");
+const medicationRoutes = require("./src/routes/medication");
+const notificationRoutes = require("./src/routes/notification");
+const reminderRoutes = require("./src/routes/reminder");
+
+
+
+app.use("/api/auth", authRoutes);
+app.use("/api/medications", medicationRoutes);
+app.use("/api/notifications", notificationRoutes);
+app.use("/api/reminders", reminderRoutes);
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Medicatie Herinneringsapp API is running!");
+});
+
+// Start server
 const PORT = process.env.PORT || 4000;
 
 sequelize.sync().then(() => {
+  console.log("✅ Tabellen gesynchroniseerd");
   app.listen(PORT, () => {
-    console.log(`Server draait op poort ${PORT}`);
+    console.log(`✅ Server draait op poort ${PORT}`);
   });
 });
